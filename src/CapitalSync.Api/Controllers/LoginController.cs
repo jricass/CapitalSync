25b19c5c43ee25a7475a560e56b31e77b26b02e9
Ricardo <jricardo.sa16@gmail.com>
2026-02-27 09:57:23 -0300

    docs(api): add endpoint summaries and descriptions for Login, Register, and GetProfile

--- src/CapitalSync.Api/Controllers/LoginController.cs
+++ src/CapitalSync.Api/Controllers/LoginController.cs
@@ -10,14 +10,16 @@ namespace CapitalSync.Api.Controllers;
 [Route("api/[controller]")]
 public class LoginController : ControllerBase
 {
-     /*
-    Por questões de segurança, informações sensíveis devem ser passadas
-    no corpo da requisição. Por isso o POST. Outros métodos não aceitam
-    injeções no Body.
-    */
+    /*
+   Por questões de segurança, informações sensíveis devem ser passadas
+   no corpo da requisição. Por isso o POST. Outros métodos não aceitam
+   injeções no Body.
+   */
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+    [EndpointSummary("User Login - Auth")]
+    [EndpointDescription("Receives login credentials from the request, returns an Token if the credentials are valid.")]
     public async Task<IActionResult> Login(
         [FromServices] IDoLoginUseCase useCase,
         [FromBody] RequestLoginUserJson request)
--- src/CapitalSync.Api/Controllers/UserController.cs
+++ src/CapitalSync.Api/Controllers/UserController.cs
@@ -15,6 +15,8 @@ public class UserController : ControllerBase
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    [EndpointSummary("User Register")]
+    [EndpointDescription("Executes the registration use case, and returns created response with the registered user details.")]
     public async Task<IActionResult> Register(
         [FromServices] IRegisterUserUseCase useCase,
         [FromBody] RequestRegisterUserJson request)
@@ -27,6 +29,8 @@ public class UserController : ControllerBase
     [HttpGet]
     [Authorize]
     [ProducesResponseType(typeof(ResponseUserProfileJson), StatusCodes.Status200OK)]
+    [EndpointSummary("Get User Profile")]
+    [EndpointDescription("Retrieves the authenticated user's profile information.")]
     public async Task<IActionResult> GetProfile([FromServices] IGetUserProfileUseCase useCase)
     {
         var response = await useCase.Execute();
